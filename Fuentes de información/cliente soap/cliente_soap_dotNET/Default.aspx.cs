18ed6a9b21d6a2fb91b9f8118dd603f00ac0c88b
camilamarin <camilamarin@Camilas-Mac-Pro.local>
2020-09-17 02:10:56 -0300

    Update README.md + Upload project folder

---
+++ Fuentes de información/cliente soap/cliente_soap_dotNET/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using camara_diputado;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private camara_diputado.WSDiputadoSoapClient cd_SoapClient;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        cd_SoapClient = new WSDiputadoSoapClient();
+
+        ListarDiputados();
+    }
+
+    private void ListarDiputados()
+    {
+        camara_diputado.Diputado diputado;
+
+        try
+        {
+            diputado = cd_SoapClient.retornarDiputado(208);
+
+            Response.Write("Nombre:" + diputado.Nombre + "<br />");
+            Response.Write("Paterno:" + diputado.ApellidoPaterno + "<br />");
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex.Message);
+        }
+    }
+}
